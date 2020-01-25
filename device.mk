ba8a39986345047a1de0f07f2bea5f7aca6382c6
chandra chaganti <chandra.chaganti@hotmail.com>
2020-01-25 15:38:22 +0530

    guacamole: PixelExperience Bringup

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/oneplus/guacamole/guacamole-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 # Audio
 PRODUCT_COPY_FILES += \
