7854e7d387cb7049b11fb4ea08210778775f224b
Chandra Chaganti <chandra.chaganti@hotmail.com>
2020-10-01 09:38:31 +0200

    guacamole: Update fingerprint to coral R

--- init/init_guacamole.cpp
+++ init/init_guacamole.cpp
@@ -57,5 +57,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys");
 }
